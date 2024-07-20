7c5d00331e1a6a06bf4cac1810614f87b23251c5
msroute42 <msroute42@gmail.com>
2024-07-20 20:39:54 +0300

    gittesting

---
+++ docs/server/chunk-4OWELNPP.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a as r,b as i,c as n,d as p,e as m,l as s,n as f,o as a,r as c,x as d}from"./chunk-2LCTFECS.mjs";var l=(()=>{let t=class t{constructor(){this.title="git-testing"}};t.\u0275fac=function(e){return new(e||t)},t.\u0275cmp=r({type:t,selectors:[["app-root"]],standalone:!0,features:[m],decls:2,vars:0,template:function(e,y){e&1&&(i(0,"h1"),p(1,"testttt"),n())}});let o=t;return o})();var u=[];var g={providers:[d(u),a()]};var x={providers:[c()]},v=s(g,x);var h=()=>f(l,v),q=h;export{q as a};
---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-4OWELNPP.mjs";import"./chunk-2LCTFECS.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
